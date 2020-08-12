f35f9ce903a5938640bd4a0656fa7a663c26b143
KoenHan <sliver1988324@gmail.com>
2020-08-12 22:24:56 +0900

    色々修正

--- code_typing_app/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// url入力ページ(トップページ)
-router.get('/', async (req, res, next) => {
-  res.render('index', { title: 'code_typingへようこそ' });
-});
-
-module.exports = router;
