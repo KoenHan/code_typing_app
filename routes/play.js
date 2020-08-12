8adfec4ed4365c9464e5ef4385d6361f14323e9b
KoenHan <sliver1998324@gmail.com>
2020-08-13 01:27:40 +0900

    サンプルテキスト追加

--- routes/play.js
+++ routes/play.js
@@ -7,11 +7,7 @@ router.post('/', async (req, res, next) => {
   const path = "[id^=LC]";
   const url = req.body.url;
   const texts = await get_texts(url, path);
-<<<<<<< HEAD:routes/play.js
-  res.render('play', { title: 'Code Typing', texts: texts, url: url});
-=======
-  res.render('play', { title: 'タイピング練習ページ', texts: texts, url: url});
->>>>>>> 9857b9c37087484f1117089e727a7f60538c6f47:code_typing_app/routes/play.js
+  res.render('play', { title: 'Code Typing', texts: texts});
 });
 
 module.exports = router;
