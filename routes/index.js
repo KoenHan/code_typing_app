122b295ae19ff3052e6ac7708d9f764d4bc64446
KoenHan <sliver1988324@gmail.com>
2020-08-13 01:27:40 +0900

    サンプルテキスト追加

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 // url入力ページ(トップページ)
 router.get('/', async (req, res, next) => {
-  res.render('index', { title: 'code_typingへようこそ' });
+  res.render('index', {
+    title: 'Code Typing',
+    description: 'URL先のコードでタイピング練習ができるサイトです．',
+    input_explain: 'URLを入力してください．'
+  });
 });
 
 module.exports = router;
