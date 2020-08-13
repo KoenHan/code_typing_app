ce3f68a0a29a89733a6657d36b85ec7de4a617bc
KoenHan <sliver1998324@gmail.com>
2020-08-14 04:31:33 +0900

    トップページの見た目調整

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,37 @@
 var express = require('express');
 var router = express.Router();
 const get_texts = require('./scrape.js');
+const err_message = [
+  '有効なアドレスを入力してください．',
+  '見つかりませんでした．URLを確認してください．'
+]
 
 router.get('/', (req, res, next) => {
   res.render('index', { show_err: false, err_message: '' });
 });
 
+const redirect = (res, err_mes) => {
+  return res.render('index', {
+    show_err: true,
+    err_message: err_mes
+  });
+}
+
 router.post('/', async (req, res, next) => {
   const url = req.body.url;
   const dot_pos = url.lastIndexOf('.');
   const slash_pos = url.lastIndexOf('/');
-  if(dot_pos < slash_pos) {
-    res.render('index', {
-      show_err: true,
-      err_message: '有効なアドレスを入力してください．'
-    });
-  } else {
-    const ext = url.slice(dot_pos);
-    console.log(ext);
-    const select_path = {
-      'github' : '[id^=LC]'
-    };
-    const texts = await get_texts(url, select_path['github']);
-    res.render('play', { texts: texts, ext: ext});
-  }
+  if(!url || dot_pos <= slash_pos || slash_pos == -1) return redirect(res, err_message[0]);
+
+  const ext = url.slice(dot_pos);
+  if(ext.length == 1) return redirect(res, err_message[0]);
+
+  const select_path = {
+    'github' : '[id^=LC]'
+  };
+  const texts = await get_texts(url, select_path['github']);
+  if(!texts.length) return redirect(res, err_message[1]);
+  return res.render('play', { texts: texts, ext: ext});
 });
 
 module.exports = router;
